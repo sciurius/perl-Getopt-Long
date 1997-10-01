52c3139be7b4c510c399db224c4ff63340c2b59f
Johan Vromans <jvromans@squirrel.nl>
1997-10-01 11:38:28 +0000

    Use "?" as alias for "help". Move POD section to end of file.

--- examples/skel2.pl
+++ examples/skel2.pl
@@ -4,9 +4,70 @@ my $RCS_Id = '$Id$ ';
 # Author          : Johan Vromans
 # Created On      : Sun Sep 15 18:39:01 1996
 # Last Modified By: Johan Vromans
-# Last Modified On: Wed Sep 17 12:53:59 1997
-# Update Count    : 10
+# Last Modified On: Wed Oct  1 13:38:13 1997
+# Update Count    : 11
 # Status          : Unknown, Use with caution!
+
+
+################ Common stuff ################
+
+# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
+# unshift (@INC, $LIBDIR);
+# require 'common.pl';
+
+use strict;
+
+my $my_package = 'Sciurix';
+my ($my_name, $my_version) = $RCS_Id =~ /: (.+).pl,v ([\d.]+)/;
+$my_version .= '*' if length('$Locker$ ') > 12;
+
+################ Program parameters ################
+
+my $verbose = 0;
+my ($debug, $trace, $test) = (0, 0, 0);
+options();
+
+################ Presets ################
+
+my $TMPDIR = $ENV{'TMPDIR'} || '/usr/tmp';
+
+################ The Process ################
+
+exit 0;
+
+################ Subroutines ################
+
+sub options () {
+    my $help = 0;		# handled locally
+    my $ident = 0;		# handled locally
+    my $man = 0;		# handled locally
+
+    # Process options. Load Getopt::Long only if needed.
+    if ( @ARGV > 0 && $ARGV[0] =~ /^[-+]/ ) {
+	# The next require / import is equivalent to "use Getopt::Long".
+	require "Getopt/Long.pm";
+	import Getopt::Long 2.0;
+	GetOptions('ident'	=> \$ident,
+		   'verbose'	=> \$verbose,
+		   'trace'	=> \$trace,
+		   'help|?'	=> \$help,
+		   'man'	=> \$man,
+		   'debug'	=> \$debug)
+	  or pod2usage(2);
+    }
+    if ( $ident or $help or $man ) {
+	print STDERR ("This is $my_package [$my_name $my_version]\n");
+    }
+    if ( $man or $help ) {
+	# Load Pod::Usage only if needed.
+	require "Pod/Usage.pm";
+	import Pod::Usage;
+	pod2usage(1) if $help;
+	pod2usage(VERBOSE => 2) if $man;
+    }
+}
+
+__END__
 
 ################ Documentation ################
 
@@ -56,61 +117,3 @@ B<This program> will read the given input file(s) and do someting
 useful with the contents thereof.
 
 =cut
-
-################ Common stuff ################
-
-# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
-# unshift (@INC, $LIBDIR);
-# require 'common.pl';
-
-use strict;
-
-my $my_package = 'Sciurix';
-my ($my_name, $my_version) = $RCS_Id =~ /: (.+).pl,v ([\d.]+)/;
-$my_version .= '*' if length('$Locker$ ') > 12;
-
-################ Program parameters ################
-
-my $verbose = 0;
-my ($debug, $trace, $test) = (0, 0, 0);
-options();
-
-################ Presets ################
-
-my $TMPDIR = $ENV{'TMPDIR'} || '/usr/tmp';
-
-################ The Process ################
-
-exit 0;
-
-################ Subroutines ################
-
-sub options () {
-    my $help = 0;		# handled locally
-    my $ident = 0;		# handled locally
-    my $man = 0;		# handled locally
-
-    # Process options. Load Getopt::Long only if needed.
-    if ( @ARGV > 0 && $ARGV[0] =~ /^[-+]/ ) {
-	# The next require / import is equivalent to "use Getopt::Long".
-	require "Getopt/Long.pm";
-	import Getopt::Long 2.0;
-	GetOptions('ident'	=> \$ident,
-		   'verbose'	=> \$verbose,
-		   'trace'	=> \$trace,
-		   'help'	=> \$help,
-		   'man'	=> \$man,
-		   'debug'	=> \$debug)
-	  or pod2usage(2);
-    }
-    if ( $ident or $help or $man ) {
-	print STDERR ("This is $my_package [$my_name $my_version]\n");
-    }
-    if ( $man or $help ) {
-	# Load Pod::Usage only if needed.
-	require "Pod/Usage.pm";
-	import Pod::Usage;
-	pod2usage(1) if $help;
-	pod2usage(VERBOSE => 2) if $man;
-    }
-}
