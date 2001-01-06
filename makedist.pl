ceec747d4a45fb0d5417fddb11d582d04ff435a0
Johan Vromans <jvromans@squirrel.nl>
2001-01-06 15:54:23 +0000

    Handle VERSION_STRING next to VERSION.

--- makedist.pl
+++ makedist.pl
@@ -4,8 +4,8 @@ my $RCS_Id = '$Id$ ';
 # Author          : Johan Vromans
 # Created On      : Tue Sep 15 15:59:04 1992
 # Last Modified By: Johan Vromans
-# Last Modified On: Fri Oct 22 15:41:10 1999
-# Update Count    : 44
+# Last Modified On: Sat Jan  6 16:54:02 2001
+# Update Count    : 51
 # Status          : Unknown, Use with caution!
 
 ################ Common stuff ################
@@ -27,7 +27,6 @@ $my_version .= '*' if length('$Locker$ ') > 12;
 ################ Command line parameters ################
 
 use Getopt::Long 2.13;
-sub app_options();
 
 my $make = 1;			# run perl/make
 my $verbose = 0;		# verbose processing
@@ -62,39 +61,39 @@ if ( $make ) {
 
 ################ Subroutines ################
 
-sub combine ($) {
+sub combine {
     my ($file) = @_;
     my $line;
-
+    my $version;
     open (F, $file) or die ("Cannot open $file [$!]\n");
     while ( defined ($line = <F>) ) {
 	if ( $line =~ /^(\s*\$VERSION\s*=\s*)(.+);\s*$/ ) {
 	    my ($pre) = $1;
-	    my $v = eval ('my $VERSION;'.$line.';$VERSION');
-	    my ($maj, $min) = ($v =~ /^(\d+)\.(\d+)$/);
+	    $version = eval ('my $VERSION;'.$line.';$VERSION');
+	    my ($maj, $min) = ($version =~ /^(\d+)\.([\d_]+)$/);
 	    $min++;
 	    print STDERR ($line, 
 			  "New version: [$maj.$min] ");
-	    chomp ($v = <STDIN>);
-	    $v = "$maj.$min" if $v eq '';
-	    $line = $pre . "\"$v\";\n";
+	    chomp ($version = <STDIN>);
+	    $version = "$maj.$min" if $version eq '';
+	    $line = $pre . "$version;\n";
+	}
+	elsif ( $line =~ /^(\s*\$VERSION_STRING\s*=\s*)(.+);\s*$/ ) {
+	    $line = $1 . "\"$version\";\n";
 	}
 	print OUT ($line);
     }
     close (F);
 }
 
-sub app_ident;
-sub app_usage($);
-
-sub app_options() {
+sub app_options {
     my $help = 0;		# handled locally
     my $ident = 0;		# handled locally
 
     # Process options, if any.
     # Make sure defaults are set before returning!
     return unless @ARGV > 0;
-    
+
     if ( !GetOptions(
 		     'make!'	=> \$make,
 		     'ident'	=> \$ident,
@@ -106,14 +105,14 @@ sub app_options() {
     {
 	app_usage(2);
     }
-    app_ident if $ident;
+    app_ident() if $ident;
 }
 
 sub app_ident {
     print STDERR ("This is $my_package [$my_name $my_version]\n");
 }
 
-sub app_usage($) {
+sub app_usage {
     my ($exit) = @_;
     app_ident;
     print STDERR <<EndOfUsage;
