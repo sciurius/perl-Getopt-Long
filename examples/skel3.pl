5f233000522a2b4d3e6f21cfe36728fba5874b13
Johan Vromans <jvromans@squirrel.nl>
2003-05-19 15:41:39 +0000

    New skeleton program for 2.33.

--- examples/skel3.pl
+++ examples/skel3.pl
@@ -6,18 +6,13 @@ my $RCS_Id = '$Id$ ';
 # Author          : Johan Vromans
 # Created On      : Tue Sep 15 15:59:04 1992
 # Last Modified By: Johan Vromans
-# Last Modified On: Fri Oct 22 15:29:52 1999
-# Update Count    : 42
+# Last Modified On: Thu May 15 14:30:11 2003
+# Update Count    : 73
 # Status          : Unknown, Use with caution!
 
 ################ Common stuff ################
 
 use strict;
-
-# Package or program libraries, if appropriate.
-# $LIBDIR = $ENV{'LIBDIR'} || '/usr/local/lib/sample';
-# use lib qw($LIBDIR);
-# require 'common.pl';
 
 # Package name.
 my $my_package = 'Sciurix';
@@ -28,12 +23,9 @@ $my_version .= '*' if length('$Locker$ ') > 12;
 
 ################ Command line parameters ################
 
-use Getopt::Long 2.13;
+my $verbose = 0;		# more verbosity
 
-# Command line options.
-my $verbose = 0;		# verbose processing
-
-# Development options (not shown with -help).
+# Development options (not shown with --help).
 my $debug = 0;			# debugging
 my $trace = 0;			# trace (show process)
 my $test = 0;			# test mode.
@@ -54,39 +46,84 @@ exit 0;
 
 ################ Subroutines ################
 
+################ Command Line Options ################
+
+use Getopt::Long 2.33;		# will enable help/version
+
 sub app_options {
-    my $help = 0;		# handled locally
-    my $ident = 0;		# handled locally
 
-    # Process options, if any.
-    # Make sure defaults are set before returning!
-    return unless @ARGV > 0;
+    GetOptions(ident	=> \&app_ident,
+	       verbose	=> \$verbose,
+	       # application specific options go here
 
-    if ( !GetOptions(
-		     'ident'	=> \$ident,
-		     'verbose'	=> \$verbose,
-		     'trace'	=> \$trace,
-		     'help|?'	=> \$help,
-		     'debug'	=> \$debug,
-		    ) or $help )
-    {
-	app_usage(2);
-    }
-    app_ident() if $ident;
+	       # development options
+	       test	=> \$test,
+	       trace	=> \$trace,
+	       debug	=> \$debug)
+      or Getopt::Long::HelpMessage(2);
 }
 
 sub app_ident {
-    print STDERR ("This is $my_package [$my_name $my_version]\n");
+    print STDOUT ("This is $my_package [$my_name $my_version]\n");
 }
 
-sub app_usage {
-    my ($exit) = @_;
-    app_ident();
-    print STDERR <<EndOfUsage;
-Usage: $0 [options] [file ...]
-    -help		this message
-    -ident		show identification
-    -verbose		verbose information
-EndOfUsage
-    exit $exit if defined $exit && $exit != 0;
-}
+__END__
+
+=head1 NAME
+
+sample - skeleton for Getopt::Long applications
+
+=head1 SYNOPSIS
+
+sample [options] [file ...]
+
+Options:
+   --ident		show identification
+   --help		brief help message
+   --verbose		verbose information
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<--verbose>
+
+More verbose information.
+
+=item B<--version>
+
+Print a version identification to standard output and exits.
+
+=item B<--help>
+
+Print a brief help message to standard output and exits.
+
+=item B<--ident>
+
+Prints a program identification.
+
+=item I<file>
+
+Input file(s).
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input file(s) and do someting
+useful with the contents thereof.
+
+=head1 AUTHOR
+
+Johan Vromans <jvromans@squirrel.nl>
+
+=head1 COPYRIGHT
+
+This programs is Copyright 2003, Squirrel Consultancy.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the Perl Artistic License or the GNU General
+Public License as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+=cut
