87778dc01cd1dbcb126e528988d3f2347aa09b87
Johan Vromans <jvromans@squirrel.nl>
2006-03-07 11:04:49 +0000

    Actualise.

--- examples/skel3.pl
+++ examples/skel3.pl
@@ -6,8 +6,8 @@ my $RCS_Id = '$Id$ ';
 # Author          : Johan Vromans
 # Created On      : Tue Sep 15 15:59:04 1992
 # Last Modified By: Johan Vromans
-# Last Modified On: Sun Sep 21 13:21:36 2003
-# Update Count    : 77
+# Last Modified On: Tue Mar  7 12:04:33 2006
+# Update Count    : 78
 # Status          : Unknown, Use with caution!
 
 ################ Common stuff ################
@@ -120,7 +120,7 @@ Johan Vromans <jvromans@squirrel.nl>
 
 =head1 COPYRIGHT
 
-This programs is Copyright 2003, Squirrel Consultancy.
+This programs is Copyright 2006,2003, Squirrel Consultancy.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the Perl Artistic License or the GNU General
